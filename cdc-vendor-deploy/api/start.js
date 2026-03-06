e604d27f2677ed0a662e1cb7489d4c9794a06aa2
fo1-oss <fo1@crepdogcrew.com>
2026-03-06 13:18:41 +0530

    Delete cdc-vendor-deploy directory

--- cdc-vendor-deploy/api/start.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const app = require("./index.js");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`CDC Vendor Portal running at http://localhost:${PORT}`);
-});
